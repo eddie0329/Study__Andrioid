02fcd8e453a2a7b5dcd5f3102a4ebf9ea04619b5
eddie0329 <fdsa0329@naver.com>
2019-06-29 19:45:08 +0900

    1st and 2nd lecture ended

---
+++ MyApplication1/app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//need to write on the terminal
+//sudo chown eddie /dev/kvm
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
